321289aa4c78e2394e546d671bd2445bcb6cc6cb
justangel <admin@just-gaming.ru>
2018-08-11 14:34:22 +0300

    Зебра

---
+++ Zebra.java
@@ -0,0 +1,5 @@
+public class Zebra {
+    public Zebra() {
+        System.out.println("Марти");
+    } // public Zebra()
+}
--- Zoo.java
+++ Zoo.java
@@ -1,5 +1,6 @@
 public class Zoo {
     public static void main(String ... args) {
         System.out.println("Добро пожаловать в Зоопарк.");
+        Zebra zebra = new Zebra();
     } //public static void main(String ... args)
 }
