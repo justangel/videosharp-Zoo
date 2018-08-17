e147cffa61bd2a2edbf18ddb64d7a93f5e5bf575
justangel <admin@just-gaming.ru>
2018-08-17 19:18:25 +0300

    Малмен

--- Zoo.java
+++ Zoo.java
@@ -3,5 +3,6 @@ public class Zoo {
         System.out.println("Добро пожаловать в Зоопарк.");
         Zebra zebra = new Zebra();
         Hippo hippo = new Hippo();
+        Giraffe giraffe = new Giraffe();
     } //public static void main(String ... args)
 }
