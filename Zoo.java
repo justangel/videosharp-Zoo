43d5508789f3e7d4e8b94dacf9a7c72cec3b1a53
justangel <admin@just-gaming.ru>
2018-08-20 19:31:51 +0300

    Шкипер

--- Zoo.java
+++ Zoo.java
@@ -3,5 +3,6 @@ public class Zoo {
         System.out.println("Добро пожаловать в Зоопарк.");
         Zebra zebra = new Zebra();
         Hippo hippo = new Hippo();
+        Penguin penguin = new Penguin();
     } //public static void main(String ... args)
 }
